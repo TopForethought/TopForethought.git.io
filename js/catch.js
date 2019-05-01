778fb8c5665232181f5a15b01ece166578ba6801
yuanfei666 <1622636762@qq.com>
2019-05-01 11:36:01 +0800

    Site updated: 2019-05-01 11:35:53

---
+++ js/catch.js
@@ -0,0 +1,8 @@
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?5354ada1e3a788d14f1ecdae77d9c03d";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
